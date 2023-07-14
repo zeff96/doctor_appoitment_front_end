4b42cb8d87a0253d0903f1619a244490b3aa316e
josuerushanika <josuerushanika@gmail.com>
2023-07-14 16:30:04 +0200

    Update store

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
-
-// import { RouterProvider } from 'react-router-dom';
-// import { Provider } from 'react-redux';
-// import store from './redux/store';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 import App from './App';
 
@@ -13,9 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      {/* <Provider store={store}> */}
+      <Provider store={store}>
       <App />
-      {/* </Provider> */}
+       </Provider> 
     </Router>
   </React.StrictMode>,
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,11 @@
-// store
+import { configureStore } from "@reduxjs/toolkit";
+import doctorsReducer from "./doctors/doctorSlice"
+
+const store = configureStore({
+    reducer: {
+        doctors: doctorsReducer,
+    }
+})
+
+export default store
+
