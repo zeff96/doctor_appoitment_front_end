b9980435bc86b8da37f093cd8a39f53d4c39395f
josuerushanika <josuerushanika@gmail.com>
2023-07-14 13:54:41 +0200

    Add doctors slice

--- src/redux/doctors/doctorSlice.js
+++ src/redux/doctors/doctorSlice.js
@@ -1,1 +1,23 @@
-// Doctor Slice
+import Axios from 'axios';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+//URLs 
+
+const BASE_URL = 'http://localhost:3000/';
+
+const initialState = {
+    doctors: [],
+    isLoading: false,
+    error: null
+  };
+
+  // fetching doctors
+
+  export const fechgDoctors = createAsyncThunk(
+    'doctors/fechgDoctors',
+    async () => {
+      const response = await axios.get(BASE_URL);
+      return response.data;
+    },
+  );
+
