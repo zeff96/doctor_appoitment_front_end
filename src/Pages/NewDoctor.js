fb982c891f59ede5141ab290ba196618dddb7590
josuerushanika <josuerushanika@gmail.com>
2023-07-17 14:30:50 +0200

    Update doctors form

--- src/Pages/NewDoctor.js
+++ src/Pages/NewDoctor.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { UseSelector, useDispatch } from 'react-redux';
-import  { createDoctor} from '../redux/doctors/doctorSlice'
+import { createDoctor } from '../redux/doctors/doctorSlice';
 
 function NewDoctor() {
+  const dispatch = useDispatch();
+  const [name, setName] = useState('');
+  const [imageurl, setImageurl] = useState('');
+  const [address, setAddress] = useState('');
+
   return (
     <div>
       <h1>Add Doctor</h1>
-      <form className='add-form w-60'>
-        <input placeholder="City" type="text" />
-        <input placeholder="date" type="date" />
+      <form className="add-form w-60">
+        <input
+          placeholder="name"
+          value={name}
+          type="text"
+          className="form-control"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+
+        <input
+          className="form-control"
+          placeholder="Add photo.."
+          type="text"
+          value={imageurl}
+          onChange={(e) => setImageurl(e.target.value)}
+        />
+
+        <input
+          className="form-control"
+          placeholder="Address"
+          type="text"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
+
         <input placeholder="subimit" type="submit" />
+
       </form>
     </div>
   );
