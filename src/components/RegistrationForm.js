6a9781eec324830616cd0d099afcb42a4b79cad3
josuerushanika <josuerushanika@gmail.com>
2023-07-18 13:31:21 +0200

    Update form and fix linters

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,32 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function RegistrationForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
   return (
     <form onSubmit>
-        <div>
-            <label htmlFor='name'>
-                Name:
-                <input
-                  type='text'
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                />
-            </label>
-        </div>
+      <div>
+        <label htmlFor="name">
+          Name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+      </div>
 
+      <div>
+        <label htmlFor="email">
+          Email:
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+      </div>
 
-        <div>
-            <label htmlFor='email'>
-                Email:
-                <input
-                  type='email'
-                  value={emal}
-                  onChange={(e) => setName(e.target.value)}
-                />
-            </label>
-        </div>
+      <div>
+        <label htmlFor="password">
+          password:
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+      </div>
+
+      <div>
+        <label htmlFor="confirmPassword">
+          password:
+          <input
+            type="password"
+            id="confirmPassword"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </label>
+      </div>
+
+      <button type="submit">Submit</button>
     </form>
-  )
+  );
 }
 
-export default RegistrationForm
+export default RegistrationForm;
