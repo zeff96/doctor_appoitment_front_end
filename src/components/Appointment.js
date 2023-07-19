c50dea6578717265d71c9e98e6e40b34a5ce24fb
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-07-19 10:47:18 +0000

    Add appointment list page

---
+++ src/components/Appointment.js
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { fetchAppointments } from '../redux/doctors/doctorSlice';
+
+function Appointments() {
+  const navigate = useNavigate();
+  const appointments = useSelector((state) => state.doctors.appointments); // Update the selector
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchAppointments());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <ul className="appointments-container">
+        {appointments.map((appointment) => (
+          <li key={appointments.id} className="appointments-list">
+            <div className="appointments-card">
+              <div className="appointments-card-details">
+                <h5>{appointment.doctor}</h5>
+                <p>{appointment.date}</p>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Appointments;
