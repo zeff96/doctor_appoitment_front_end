a2db632c65ca24b7e3ce01e4e82258dcb70c1604
josuerushanika <josuerushanika@gmail.com>
2023-07-20 15:32:59 +0200

    Add style

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -2,6 +2,7 @@ import { NavLink, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { useRef } from 'react';
 import { loginAsync } from '../redux/users/userSlice';
+import '../css/LoginPage.css';
 
 function LoginPage() {
   const formRef = useRef();
@@ -17,18 +18,18 @@ function LoginPage() {
       user: { email: form.email, password: form.password },
     };
 
-    dispatch(loginAsync(data).then((result) => {
+    dispatch(loginAsync(data)).then((result) => {
       if (result.payload === undefined) return;
       navigate('/doctors');
-    }));
+    });
     e.target.reset();
   }
 
   return (
-    <div>
-      <form ref={formRef} onSubmit={handleSubmit}>
-        <input type="email" name="email" id="email" required placeholder="email" className="form-control mb-3" />
-        <input type="password" name="password" id="password" required placeholder="password" className="form-control mb-3" />
+    <div className="div-container">
+      <form ref={formRef} onSubmit={handleSubmit} className="">
+        <input type="email" name="email" id="email" required placeholder="email" className="" />
+        <input type="password" name="password" id="password" required placeholder="password" className="" />
         <input type="submit" value="Log in" className="btn btn-primary" />
       </form>
       <div>
