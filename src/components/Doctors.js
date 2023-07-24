5cba5f42537fd7e261716806dbea3538e9a8c247
josuerushanika <josuerushanika@gmail.com>
2023-07-24 21:33:50 +0200

    Add css

--- src/components/Doctors.js
+++ src/components/Doctors.js
@@ -1,6 +1,8 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+// import { BiLogoYoutube } from 'react-icons/im';
+import { BiLogoYoutube, BiLogoFacebookSquare, BiLogoInstagram } from 'react-icons/bi';
 import { fechDoctors } from '../redux/doctors/doctorSlice';
 import Navbar from './Navbar';
 import '../css/Home.css';
@@ -16,30 +18,38 @@ const Doctors = () => {
   return (
     <div>
       <Navbar />
-      <ul className="doctors-container">
-        {doctors.map((doctor) => (
-          <li key={doctor.id} className="doctors-list">
-            <button
-              className="doctors-card"
-              type="button"
-              onClick={() => {
-                navigate(`${doctor.id}`);
-              }}
-            >
-              <div className="circle-color">
-                <img src={doctor.image_url} alt={doctor.name} className="rounded-circle w-50 h-20 blob" width="2" />
-              </div>
-              <div className="doctors-card-details">
-                <h5>{doctor.name}</h5>
-                <p className="dots2">..............................</p>
-                <p className="doctors-details">
-                  {doctor.bio}
-                </p>
-              </div>
-            </button>
-          </li>
-        ))}
-      </ul>
+      <h1> LAST MODEL </h1>
+      <div className="card-group">
+        <div className="card">
+          <ul className="doctors-container">
+            {doctors.map((doctor) => (
+              <li key={doctor.id} className="doctors-list">
+                <button
+                  className="doctors-card"
+                  type="button"
+                  onClick={() => {
+                    navigate(`${doctor.id}`);
+                  }}
+                >
+                  <div className="circle-color">
+                    <img src={doctor.image_url} alt={doctor.name} className="rounded-circle w-50 h-20 blob" width="2" />
+                  </div>
+                  <div className="doctors-card-details">
+                    <h5>{doctor.name}</h5>
+                    <p className="dots2">..............................</p>
+                    <p className="doctors-details">
+                      {doctor.bio}
+                    </p>
+                  </div>
+                  <BiLogoYoutube />
+                  <BiLogoFacebookSquare />
+                  <BiLogoInstagram />
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
