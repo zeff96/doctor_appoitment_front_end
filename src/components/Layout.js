0d6179e5ccfddb51cdecfbd7b54965c19658955a
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-07-25 09:00:26 +0000

    Style layout and navbar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,11 +3,14 @@ import Navbar from './Navbar';
 
 const Layout = () => (
   <div className="container-fluid">
-    <div className="row main-page">
-      <div className="col-3">
+    <div className="d-flex main-page">
+      <div className="d-flex" style={{ maxWidth: '20vw' }}>
         <Navbar />
+        <button className="btn card d-flex align-item-end justify-content-end mb-5 mt-auto border rounded-end-5 bg-info" type="button" data-bs-toggle="collapse" data-bs-target="#collapseElement" aria-expanded="false" aria-controls="collapseElement">
+          <i className="bi bi-caret-right-fill text-light" />
+        </button>
       </div>
-      <div className="col-9 d-flex align-items-center">
+      <div className="d-flex align-items-center m-auto" style={{ height: '100vh' }}>
         <Outlet />
       </div>
     </div>
