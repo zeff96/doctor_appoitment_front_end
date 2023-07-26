06cc1ad140e8622284b7a81cb9eba094b2dfe27d
josuerushanika <josuerushanika@gmail.com>
2023-07-26 08:47:15 +0200

    Update test

--- src/__test__/Doctors.test.js
+++ src/__test__/Doctors.test.js
@@ -3,15 +3,11 @@ import React from "react";
 import { render } from '@testing-library/react';
 // import { Provider } from 'react-redux';
 // import { store } from "../redux/store";
-// import Doctors from "../components/Doctors";
+ import Doctors from "../components/Doctors";
 
 describe('Home test for  rendering', () => {
     test('render all rendering from api', () => {
-      const Doctors = render(
-       
-            <  Doctors />
-        
-      );
+      const { container } = render(<Doctors />);
       expect(Doctors).toMatchSnapshot();
     });
   });
