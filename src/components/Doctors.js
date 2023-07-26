50e8f3168ea19f2c4bb2d2f45754dd6d8f4480d9
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-07-26 09:00:48 +0000

    Implement carousel

--- src/components/Doctors.js
+++ src/components/Doctors.js
@@ -1,8 +1,16 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import Carousel from 'react-elastic-carousel';
 import { useNavigate } from 'react-router-dom';
 import { fechDoctors } from '../redux/doctors/doctorSlice';
 import { useAppDispatch, useAppSelector } from '../hooks/hooks';
+
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 170, itemsToShow: 2 },
+  { width: 550, itemsToShow: 2 },
+  { width: 768, itemsToShow: 3 },
+  { width: 1200, itemsToShow: 4 },
+];
 
 const Doctors = () => {
   const navigate = useNavigate();
@@ -13,49 +21,37 @@ const Doctors = () => {
   }, [dispatch]);
 
   return (
-    <div className="d-flex flex-column">
+    <div className="vh-100 d-flex flex-column justify-content-center">
       {/* <Navbar /> */}
-      <h1 className="h1 d-flex justify-content-center">Qualified Doctor</h1>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <div className="carousel slide" id="carouselExampleIndicators" data-ride="carousel">
-              <ol className="carousel-indicators">
-                <li className="active" data-target="#carouselExampleIndicators" data-slide-to="0" />
-                <li data-target="#carouselExampleIndicators" data-slide-to="1" />
-              </ol>
-              <div className="carousel-inner">
-                <div className="carousel-item active">
-                  <ul className="row list-group list-group-flush list-group-horizontal">
-                    {doctors.map((doctor) => (
-                      <li key={doctor.id} className="col-md-4 list-group-item">
-                        <button
-                          className="doctors-card card single-box"
-                          type="button"
-                          onClick={() => {
-                            navigate(`${doctor.id}`);
-                          }}
-                        >
-                          <div className="circle-color card-body img-area">
-                            <img src={doctor.image_url} alt={doctor.name} className="rounded-circle card-img-top" height="300px" />
-                          </div>
-                          <div className="doctors-card-details img-text">
-                            <h5>{doctor.name}</h5>
-                            <hr />
-                            <p className="doctors-details">
-                              {doctor.bio}
-                            </p>
-                          </div>
-                        </button>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
+      <h1 className="h1 d-flex justify-content-center mb-5">Qualified Doctor</h1>
+      <Carousel breakPoints={breakPoints}>
+        {doctors.map((doctor) => (
+          <button
+            key={doctor.id}
+            className="doctors-card card border-white"
+            type="button"
+            onClick={() => {
+              navigate(`${doctor.id}`);
+            }}
+          >
+            <div className="circle-color card-body img-area d-flex m-auto">
+              <img src={doctor.image_url} alt={doctor.name} className="rounded card-img-top" height="200px" />
+            </div>
+            <div className="doctors-card-details img-text">
+              <h5>{doctor.name}</h5>
+              <hr />
+              <p className="doctors-details">
+                {doctor.bio}
+              </p>
+              <div className="social-media">
+                <a href={doctor.social_media.facebook} aria-label="Facebook" target="_blank" rel="noreferrer"><i className="bi bi-facebook p-1 mx-2 h5 text-dark" /></a>
+                <a href={doctor.social_media.twitter} aria-label="Twitter" target="_blank" rel="noreferrer"><i className="bi bi-twitter p-1 mx-2 h5 text-dark" /></a>
+                <a href={doctor.social_media.instagram} aria-label="Instagram" target="_blank" rel="noreferrer"><i className="bi bi-instagram p-1 mx-2 h5 text-dark" /></a>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
+          </button>
+        ))}
+      </Carousel>
     </div>
   );
 };
