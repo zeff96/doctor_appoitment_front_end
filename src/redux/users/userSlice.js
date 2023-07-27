b230db347553831c76e9b533b982fe46a9ce0fb5
zeff96 <zeffo1975@gmail.com>
2023-07-27 18:57:15 +0300

    Update url for hosted server

--- src/redux/users/userSlice.js
+++ src/redux/users/userSlice.js
@@ -12,7 +12,7 @@ const initialState = {
   passwordResetNewError: null,
 };
 
-const url = 'http://127.0.0.1:3000';
+const url = 'https://doctor-api-3dvk.onrender.com';
 
 export const signUpAsync = createAsyncThunk(
   'signup/Async',
