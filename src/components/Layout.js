f49fd0028b193df42405b4a309c0eade05f6c408
mutai-gilbert <mutaigilbert85@gmail.com>
2023-07-27 21:13:38 +0300

    Added new changes to the Layout.js file

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,13 +4,13 @@ import Navbar from './Navbar';
 const Layout = () => (
   <div className="container-fluid">
     <div className="row main-page">
-      <div className="col-4 col-xxl-3 col-xl-3 col-lg-4 col-md-4 col-sm-6 col-xm-6" style={{ height: '100vh' }}>
+      <div className="col-3 col-xxl-3 col-xl-3 col-lg-3 col-md-3 col-sm-6 col-xm-6" style={{ height: '100vh' }}>
         <Navbar />
         <button className="d-flex btn card align-item-end justify-content-end mb-5 mt-auto border rounded-end-5 bg-info" type="button" data-bs-toggle="collapse" data-bs-target="#collapseElement" aria-expanded="false" aria-controls="collapseElement">
           <i className="bi bi-caret-right-fill text-light" />
         </button>
       </div>
-      <div className="col-6 col-xxl-9 col-xl-8 col-lg-8 col-md-8 col-sm-6 col-xm-6 mx-auto py-6" style={{ height: '100vh' }}>
+      <div className="col-9 col-xxl-9 col-xl-9 col-lg-9 col-md-9 col-sm-6 col-xm-6 mx-auto py-6" style={{ height: '100vh' }}>
         <Outlet />
       </div>
     </div>
