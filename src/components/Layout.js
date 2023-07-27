c02a0b274cc1d620c60d42667dabedda670130a6
mutai-gilbert <mutaigilbert85@gmail.com>
2023-07-27 21:51:28 +0300

    Added new changes to the Layout.js file

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,20 +1,41 @@
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 
-const Layout = () => (
-  <div className="container-fluid">
-    <div className="row main-page">
-      <div className="col-3 col-xxl-3 col-xl-3 col-lg-3 col-md-3 col-sm-6 col-xm-6" style={{ height: '100vh' }}>
-        <Navbar />
-        <button className="d-flex btn card align-item-end justify-content-end mb-5 mt-auto border rounded-end-5 bg-info" type="button" data-bs-toggle="collapse" data-bs-target="#collapseElement" aria-expanded="false" aria-controls="collapseElement">
-          <i className="bi bi-caret-right-fill text-light" />
-        </button>
-      </div>
-      <div className="col-9 col-xxl-9 col-xl-9 col-lg-9 col-md-9 col-sm-6 col-xm-6 mx-auto py-6" style={{ height: '100vh' }}>
-        <Outlet />
+const Layout = () => {
+  const [isNavbarOpen, setIsNavbarOpen] = useState(true);
+
+  const handleNavbarToggle = () => {
+    setIsNavbarOpen((prevState) => !prevState);
+  };
+
+  return (
+    <div className="container-fluid">
+      <div className="row main-page">
+        {/* Navbar column */}
+        <div className={`col-navbar col-xxl-3 col-xl-3 col-lg-4 col-md-4 col-sm-6 col-xm-6 ${isNavbarOpen ? 'open' : 'closed'}`}>
+          {/* Render Navbar when it's open */}
+          {isNavbarOpen && <Navbar />}
+
+          {/* Button to toggle Navbar */}
+          <button
+            className="navbar-button d-flex btn card align-item-end justify-content-end mb-5 mt-auto border rounded-end-5 bg-info"
+            type="button"
+            onClick={handleNavbarToggle}
+          >
+            <i className={`bi bi-caret-${isNavbarOpen ? 'right' : 'left'}-fill text-light`} />
+          </button>
+        </div>
+
+        {/* Outlet column */}
+        <div
+          className={`col-outlet-${isNavbarOpen ? 'open' : 'closed'} col-xxl-9 col-xl-9 col-lg-8 col-md-8 col-sm-6 col-xm-6 mx-auto py-6`}
+        >
+          <Outlet />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Layout;
