b629df3790c0722e00e27e1def1477302cd9be1a
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-07-27 19:57:14 +0000

    Make button postion stick on bottom

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -16,7 +16,13 @@ const Layout = () => {
         <div className={`col-navbar col-xxl-3 col-xl-3 col-lg-4 col-md-4 col-sm-6 col-xm-6 ${isNavbarOpen ? 'open' : 'closed'}`}>
           {/* Render Navbar when it's open */}
           {isNavbarOpen && <Navbar />}
+        </div>
 
+        {/* Outlet column */}
+        <div
+          className={`col-outlet-${isNavbarOpen ? 'open' : 'closed'} col-xxl-9 col-xl-9 col-lg-8 col-md-8 col-sm-6 col-xm-6 mx-auto py-6`}
+        >
+          <Outlet />
           {/* Button to toggle Navbar */}
           <button
             className="navbar-button d-flex btn card align-item-end justify-content-end mb-5 mt-auto border rounded-end-5 bg-info"
@@ -25,13 +31,6 @@ const Layout = () => {
           >
             <i className={`bi bi-caret-${isNavbarOpen ? 'right' : 'left'}-fill text-light`} />
           </button>
-        </div>
-
-        {/* Outlet column */}
-        <div
-          className={`col-outlet-${isNavbarOpen ? 'open' : 'closed'} col-xxl-9 col-xl-9 col-lg-8 col-md-8 col-sm-6 col-xm-6 mx-auto py-6`}
-        >
-          <Outlet />
         </div>
       </div>
     </div>
